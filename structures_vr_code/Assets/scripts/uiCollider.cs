d4c5f5192904f46273a1120d156dc042165c78c7
Kameron Heyen <kameronh7@gmail.com>
2018-10-28 15:16:28 -0500

    toggling working
    lots of errors because of trying to get components that do not exist

--- structures_vr_code/Assets/scripts/uiCollider.cs
+++ structures_vr_code/Assets/scripts/uiCollider.cs
@@ -5,13 +5,29 @@ using UnityEngine.UI;
 
 public class uiCollider : MonoBehaviour {
 	private Button button;
+	private Toggle toggle;
+	bool toggleBool = false;
 
 	void Start() {
 		button = GetComponent<Button>();
+		toggle = GetComponent<Toggle>();
+		if(toggle != null) {
+			toggle.isOn = toggleBool;
+		}
+
 	}
 
 	public void uiHit() {
-		button.onClick.Invoke();
+		if(button != null) {
+			button.onClick.Invoke();
+		} else if(toggle != null) {
+			if(toggleBool) {
+				toggleBool = false;
+			} else {
+				toggleBool = true;
+			}
+			//toggle.onValueChanged.Invoke(toggleBool);
+			toggle.isOn = toggleBool;
+		}
     }
-
 }
