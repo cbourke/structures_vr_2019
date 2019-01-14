05337ebe28e23f5d2b038f9fb32b4694f234cd80
Dylan <d.finley@ghowardfinley.com>
2019-01-14 14:29:19 -0600

    dropdown menu working

---
+++ structures_vr_code/Assets/scripts/UIScripts/defineDropDown.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class defineDropDown : MonoBehaviour {
+
+    Dropdown m_Dropdown;
+    public GameObject material;
+    public GameObject section;
+    public GameObject group;
+
+    void Start()
+    {
+        m_Dropdown = GetComponent<Dropdown>();
+        //Add listener for when the value of the Dropdown changes, to take action
+        m_Dropdown.onValueChanged.AddListener(delegate {
+            DropdownValueChanged(m_Dropdown);
+        });
+        material.SetActive(true);
+    }
+
+    //Ouput the new value of the Dropdown into Text
+    void DropdownValueChanged(Dropdown change)
+    {
+        if (change.value == 0)
+        {
+            material.SetActive(true);
+            section.SetActive(false);
+            group.SetActive(false);
+        } else if (change.value == 1)
+        {
+            material.SetActive(false);
+            section.SetActive(true);
+            group.SetActive(false);
+        } else
+        {
+            material.SetActive(false);
+            section.SetActive(false);
+            group.SetActive(true);
+        }
+    }
+}
+
