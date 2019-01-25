391086203472d525b2322d7f541e40b2b0509400
James Griess <bobo12377@gmail.com>
2019-01-24 20:17:01 -0600

    able to select material when creating section

---
+++ structures_vr_code/Assets/materialListDropdown.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class materialListDropdown : MonoBehaviour {
+
+	public GameObject materialController;
+	public TMP_Dropdown dropdown;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	
+	void OnEnable()
+    {
+		List<BuildingMaterial> materialList = materialController.GetComponent<materialsController>().GetMaterials();
+		dropdown.ClearOptions();
+		List<string> materialNames = new List<string>();
+		
+		foreach(BuildingMaterial mat in materialList)
+		{
+			string name = mat.GetName();
+			Debug.Log("name = " + name);
+			materialNames.Add(name);
+		}
+		foreach(string str in materialNames)
+		{
+			Debug.Log("names == " + str);
+		}
+		dropdown.AddOptions(materialNames);
+		//dropdown.value = 0;
+    }
+	
+
+}
