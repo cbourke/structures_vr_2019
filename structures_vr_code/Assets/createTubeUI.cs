ec0066042868fe9faf0df68f9c7932abb8230118
James Griess <11860216+JamesGriess97@users.noreply.github.com>
2019-01-25 10:02:37 -0600

    create tube works

---
+++ structures_vr_code/Assets/createTubeUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class createTubeUI : MonoBehaviour
+{
+
+    public TMP_InputField depth;
+    public TMP_InputField width;
+    public TMP_InputField flange;
+    public TMP_InputField web;
+    public TMP_InputField tubeName;
+    public TMP_Dropdown materialDropdown;
+    public GameObject sectionController;
+
+    public void createTube() {
+        string materialName = (materialDropdown.options[materialDropdown.value].text);
+        float depthVal = float.Parse(depth.text);
+        float widthVal = float.Parse(width.text);
+        float flangeVal = float.Parse(flange.text);
+        float webVal = float.Parse(web.text);
+        
+        sectionController.GetComponent<sectionController>().addTubeFrameSection(tubeName.text, materialName, depthVal, widthVal, flangeVal, webVal);
+    }
+}
