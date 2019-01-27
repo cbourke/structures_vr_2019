0a465e467c4caa3b218fecde9cc9608ed9e1caf9
James Griess <11860216+JamesGriess97@users.noreply.github.com>
2019-01-27 16:38:49 -0600

    unit conversion stuff works, conversion values should still be tested

--- structures_vr_code/Assets/scripts/debugDrawing.cs
+++ structures_vr_code/Assets/scripts/debugDrawing.cs
@@ -8,25 +8,52 @@ public class debugDrawing : MonoBehaviour {
     public GameObject xmlController;
     public GameObject materialController;
     public GameObject sectionController;
+    public GameObject unitsController;
 
-	// Use this for initialization
+    Vector3 origin = new Vector3(0,0,0);
+    Vector3 p1 = new Vector3(1,0,0);
+    Vector3 p2 = new Vector3(0,1,0);
+    Vector3 p3 = new Vector3(1,1,0);
+    Vector3 p4 = new Vector3(0,2,0);
+    Vector3 p5 = new Vector3(1,2,0);
+    Vector3 p6 = new Vector3(0,3,0);
+
+	
 	void Start () {
+        
+        //xmlController.GetComponent<xmlController>().sendFileToSap(); //send to SAP2000
+        Debug.Log("START DEBUG");
+        testConversions();
+    }
+	
+    void testConversions()
+    {
+        unitsController uC = unitsController.GetComponent<unitsController>();
+        int lengthMeter = 5;
+        int forceNewton = 15;
+        int tempCel = 20;
+        uC.setUnits("N, m, C");
+        Debug.Log("Force: " + uC.getForceUnit() + ", Length: " + uC.getLengthUnit() + ", Temp: " + uC.getTempUnit());
+        Debug.Log(tempCel + " Celcius is " + uC.getTemperature(tempCel) + " Celcius");
+        uC.setUnits("Kip, ft, F");
+        Debug.Log("Force: " + uC.getForceUnit() + ", Length: " + uC.getLengthUnit() + ", Temp: " + uC.getTempUnit());
+        
+        Debug.Log(lengthMeter + " meters is " + uC.getLength(lengthMeter) + " feet");
+        Debug.Log(forceNewton + " newtons is " + uC.getForce(forceNewton) + " Kips");
+        Debug.Log(tempCel + " Celcius is " + uC.getTemperature(tempCel) + " farenheit");
+        
+        int lengthFeet = 8;
+        int forceKips = 120;
+        int tempFaren = 75;
+        Debug.Log(lengthFeet + " feet is " + uC.getLengthMeters(lengthFeet) + " meters");
+        Debug.Log(forceKips + "  Kips is " + uC.getForceNewtons(forceKips) + " newtons");
+        Debug.Log(tempFaren + "  farenheit is " + uC.getTemperatureCelcius(tempFaren) + " Celcius");
+        
+        
+    }
 
-		Vector3 origin = new Vector3(0,0,0);
-		Vector3 p1 = new Vector3(1,0,0);
-		Vector3 p2 = new Vector3(0,1,0);
-		Vector3 p3 = new Vector3(1,1,0);
-		Vector3 p4 = new Vector3(0,2,0);
-		Vector3 p5 = new Vector3(1,2,0);
-		Vector3 p6 = new Vector3(0,3,0);
-
-        /* TODO: rewrite this so it doesn't look horrible */
-/*
-        materialController.GetComponent<materialsController>().addBuildingMaterial("Steel01", BuildingMaterialAttributes.Regions.UNITEDSTATES, BuildingMaterialAttributes.Regions.UnitedStatesTypes.STEEL, BuildingMaterialAttributes.Regions.UnitedStatesTypes.SteelStandards.A53, BuildingMaterialAttributes.Regions.UnitedStatesTypes.SteelStandards.A53Grades.GRADE_B);
-        materialController.GetComponent<materialsController>().addBuildingMaterial("Steel02", BuildingMaterialAttributes.Regions.UNITEDSTATES, BuildingMaterialAttributes.Regions.UnitedStatesTypes.STEEL, BuildingMaterialAttributes.Regions.UnitedStatesTypes.SteelStandards.A500, BuildingMaterialAttributes.Regions.UnitedStatesTypes.SteelStandards.A500Grades.GRADE_B_fy_46);
-        materialController.GetComponent<materialsController>().addBuildingMaterial("Aluminum01", BuildingMaterialAttributes.Regions.UNITEDSTATES, BuildingMaterialAttributes.Regions.UnitedStatesTypes.ALUMINUM, BuildingMaterialAttributes.Regions.UnitedStatesTypes.AluminumStandards.ASTM, BuildingMaterialAttributes.Regions.UnitedStatesTypes.AluminumStandards.ASTMGrades.GRADE_Alloy_6063_T6);
- */
-
+    void testSections()
+    {
         sectionController.GetComponent<sectionController>().addIFrameSection("Sec_Steel_I", "Steel01", 0.3, 0.12, 0.01, 0.007, 0.12, 0.01);
         sectionController.GetComponent<sectionController>().addPipeFrameSection("Sec_Steel_Pipe", "Steel02", 0.2, 0.01);
         sectionController.GetComponent<sectionController>().addTubeFrameSection("Sec_Aluminum_Tube", "Aluminum01", 0.16, 0.1, 0.007, 0.007);
@@ -45,8 +72,10 @@ public class debugDrawing : MonoBehaviour {
         Debug.Log("Current frame section: " + sectionController.GetComponent<sectionController>().GetCurrentFrameSection().GetName());
         constructorController.GetComponent<constructorController>().setPoint(p4, buildingObjects.Frame);
         constructorController.GetComponent<constructorController>().setPoint(p5, buildingObjects.Frame);
+    }
 
-
+    void testJoints()
+    {
         constructorController.GetComponent<constructorController>().createJointRestraint(origin, 'f');
         constructorController.GetComponent<constructorController>().createJointRestraint(p1, 'r');
         constructorController.GetComponent<constructorController>().createJointRestraint(p1, 'f'); // overwrite
@@ -59,11 +88,17 @@ public class debugDrawing : MonoBehaviour {
         constructorController.GetComponent<constructorController>().deleteJointRestraint(p5); // delete
 
         constructorController.GetComponent<constructorController>().createJointRestraint(p6, 'f'); // This is meant to fail, as there is no frame endpoint here
+    }
 
-        xmlController.GetComponent<xmlController>().sendFileToSap(); //send to SAP2000
-
-        // Testing BuildingMaterial objects:
-        /* 
+	void testMaterials()
+    {
+        
+        // this is broken because the way building materials is being handled has changed
+        /*
+        materialController.GetComponent<materialsController>().addBuildingMaterial("Steel01", BuildingMaterialAttributes.Regions.UNITEDSTATES, BuildingMaterialAttributes.Regions.UnitedStatesTypes.STEEL, BuildingMaterialAttributes.Regions.UnitedStatesTypes.SteelStandards.A53, BuildingMaterialAttributes.Regions.UnitedStatesTypes.SteelStandards.A53Grades.GRADE_B);
+        materialController.GetComponent<materialsController>().addBuildingMaterial("Steel02", BuildingMaterialAttributes.Regions.UNITEDSTATES, BuildingMaterialAttributes.Regions.UnitedStatesTypes.STEEL, BuildingMaterialAttributes.Regions.UnitedStatesTypes.SteelStandards.A500, BuildingMaterialAttributes.Regions.UnitedStatesTypes.SteelStandards.A500Grades.GRADE_B_fy_46);
+        materialController.GetComponent<materialsController>().addBuildingMaterial("Aluminum01", BuildingMaterialAttributes.Regions.UNITEDSTATES, BuildingMaterialAttributes.Regions.UnitedStatesTypes.ALUMINUM, BuildingMaterialAttributes.Regions.UnitedStatesTypes.AluminumStandards.ASTM, BuildingMaterialAttributes.Regions.UnitedStatesTypes.AluminumStandards.ASTMGrades.GRADE_Alloy_6063_T6);
+        
         BuildingMaterial bm1 = new BuildingMaterial();
         BuildingMaterial bm2 = new BuildingMaterial("I_am_bm_2");
         BuildingMaterial bm3 = new BuildingMaterial("Three", BuildingMaterialAttributes.Regions.UNITEDSTATES,
@@ -87,11 +122,5 @@ public class debugDrawing : MonoBehaviour {
         bm3.SetRegion(111); // Should not work; no change should be made.
         Debug.Log("bm3 (4): " + bm3.GetUserDefinedName() + ", " + bm3.GetRegion() + ", " + bm3.GetMaterialType() + ", " + bm3.GetStandard() + ", " + bm3.GetGrade());
         */
-
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
---
+++ structures_vr_code/Assets/unitsController.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text.RegularExpressions;
+using System;
+
+public class unitsController : MonoBehaviour {
+	private string length;
+	private string force;
+	private string temperature;
+
+	// how many decimal places to round to
+	private int decimalPlaces = 3;
+
+	// stores ratios for unit conversions 
+	// the system defaults to m,N,C
+	// temp conversions handled in function because its not a ratio and theres only 2
+	private Dictionary<string, double> convTable = new Dictionary<string, double>();
+
+    void Start()
+    {
+        // distance conversions
+        this.convTable.Add("m", 1.0);
+        this.convTable.Add("cm", 0.01);
+        this.convTable.Add("mm", 0.001);
+        this.convTable.Add("ft", 3.281);
+        this.convTable.Add("in", 0.0254);
+
+        // force conversions
+        this.convTable.Add("N", 1.0);
+        this.convTable.Add("lb", 0.2248);
+        this.convTable.Add("Tonf", 0.0001019716);
+        this.convTable.Add("KN", 0.001);
+        this.convTable.Add("Kip", 0.00022481);
+        this.convTable.Add("Kgf", 0.101972);
+    }
+
+    public void setUnits(string unitsString){
+		// strip whitespace
+		unitsString = Regex.Replace(unitsString, @"\s+", "");
+		// split (format: m,N,C)
+		string[] unitsArray = unitsString.Split(',');
+		
+		this.force = unitsArray[0];
+		this.length = unitsArray[1];
+		this.temperature = unitsArray[2];
+	}
+
+	// unit getters and setters
+	public string getLengthUnit() {
+		return this.length;
+	}
+
+	public void setLengthUnit(string unit) {
+		this.length = unit;
+	}
+
+	public string getForceUnit() {
+		return this.force;
+	}
+
+	public void setForceUnit(string unit) {
+		this.force = unit;
+	}
+
+	public string getTempUnit() {
+		return this.temperature;
+	}
+
+	public void setTempUnit(string unit) {
+		this.temperature = unit;
+	}
+
+	// takes length in meters and returns user defined units
+	public double getLength(double length) {
+        Debug.Log("length: " + length * this.convTable[this.length]);
+		return Math.Round(length * this.convTable[this.length], this.decimalPlaces);
+	}
+
+	// get lenght in meters given current unit
+	public double getLengthMeters(double length) {
+		return Math.Round(length / this.convTable[this.length], this.decimalPlaces);
+    }
+
+	// takes force in Newtons and returns user defined force
+	public double getForce(double force) {
+		return Math.Round(force * this.convTable[this.force], this.decimalPlaces);
+    }
+
+	// returns force in newtons
+	public double getForceNewtons(double force) {
+		return Math.Round(force / this.convTable[this.force], this.decimalPlaces);
+    }
+	
+	// takes temp in Celcius and returns user defined temp
+	public double getTemperature(double temp) {
+		if (this.temperature == "C"){
+			return temp;
+		}
+		return Math.Round((temp * 1.8f + 32), this.decimalPlaces);
+	}
+
+	// given a temp in farenheit this converts and returns it as celcius
+	public double getTemperatureCelcius(double temp){
+		return Math.Round(((temp - 32) / 1.8f), this.decimalPlaces);
+    }
+
+}
