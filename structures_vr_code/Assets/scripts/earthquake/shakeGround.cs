6b58b79f8ee13f136706056278ad53a4034e5c23
James Griess <bobo12377@gmail.com>
2019-01-30 10:26:19 -0600

    added ground shake script

--- structures_vr_code/Assets/scripts/earthquake/shakeGround.cs
+++ structures_vr_code/Assets/scripts/earthquake/shakeGround.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class shakeGround : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    public float velocidadMax;
+
+    public float xMax;
+    public float zMax;
+    public float xMin;
+    public float zMin;
         
+    private float x;
+    private float z;
+    private float tiempo;
+
+    // Use this for initialization
+    void Start () {
+        x = Random.Range(-velocidadMax, velocidadMax);
+        z = Random.Range(-velocidadMax, velocidadMax);
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update () {
+
+        tiempo += Time.deltaTime;
+
+        if (transform.localPosition.x > xMax) {
+            x = Random.Range(-velocidadMax, 0.0f);
+            tiempo = 0.0f; 
+        }
+        if (transform.localPosition.x < xMin) {
+            x = Random.Range(0.0f, velocidadMax);
+            tiempo = 0.0f; 
+        }
+        if (transform.localPosition.z > zMax) {
+            z = Random.Range(-velocidadMax, 0.0f);
+            tiempo = 0.0f; 
+        }
+        if (transform.localPosition.z < zMin) {
+            z = Random.Range(0.0f, velocidadMax);
+            tiempo = 0.0f; 
+        }
+
+
+        if (tiempo > 1.0f) {
+            x = Random.Range(-velocidadMax, velocidadMax);
+            z = Random.Range(-velocidadMax, velocidadMax);
+            tiempo = 0.0f;
+        }
+
+        transform.localPosition = new Vector3(transform.localPosition.x + x, transform.localPosition.y, transform.localPosition.z + z);
     }
 }
