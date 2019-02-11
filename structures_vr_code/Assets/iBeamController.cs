9b488914705a4b3622750de7df93b8f2e162a8d1
James Griess <bobo12377@gmail.com>
2019-02-11 11:02:34 -0600

    I beam prefab kind of works

--- structures_vr_code/Assets/iBeamController.cs
+++ structures_vr_code/Assets/iBeamController.cs
@@ -40,34 +40,32 @@ public class iBeamController : MonoBehaviour
     void Start()
     {
         trans = GetComponent<Transform>();
+        SetDimensions((float) 0.5, (float) 0.5, (float) 0.2, (float)0.2, (float)2.0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //SetDimensions((float) 0.5, (float) 0.5, (float) 0.2, (float)0.2, (float)2.0);
-        SetDimensions(depth, flangeWidth, flangeThickness, webThickness, length);
+        SetDimensions(depth, flangeWidth, flangeThickness, webThickness,  length);
     }
 
     void SetDimensions(float d, float flangeW, float flangeT, float webT, float length)
     {
         this.depth = d;
-        this.webThickness = webT;
         this.flangeWidth = flangeW;
         this.flangeThickness = flangeT;
+        this.webThickness = webT;
         this.length = length;
 
         Vector3 webScale = new Vector3(webThickness, depth, length);
-
         web.GetComponent<Transform>().localScale = webScale;
 
-        Vector3 topScale = new Vector3(flangeThickness, flangeWidth, length);
-
-        topFlange.GetComponent<Transform>().localScale = topScale;
-
-        Vector3 bottomScale = new Vector3(flangeThickness, flangeWidth, length);
-
-        bottomFlange.GetComponent<Transform>().localScale = bottomScale;
+        Vector3 topScale = new Vector3(flangeWidth, flangeThickness,  length);
+        topFlange.GetComponent<Transform>().localScale = new Vector3 (topScale.x / webScale.x, topScale.y / webScale.y, topScale.z / webScale.z);
+        topFlange.GetComponent<Transform>().localPosition = new Vector3 (0, (float)0.5 - (flangeThickness / 2)/webScale.y, 0);
+        Vector3 bottomScale = new Vector3(flangeWidth, flangeThickness, length);
+        bottomFlange.GetComponent<Transform>().localScale = new Vector3 (bottomScale.x / webScale.x, bottomScale.y / webScale.y, bottomScale.z / webScale.z);
+        bottomFlange.GetComponent<Transform>().localPosition = new Vector3 (0, (float)-0.5 + (flangeThickness / 2)/webScale.y, 0);
 
     }
 }
