06e7658c60a53ddd3b787e7ee794a54490f196c9
James Griess <bobo12377@gmail.com>
2019-02-20 23:03:21 -0600

    release indicators working for all section types

--- structures_vr_code/Assets/scripts/frame_TubeController.cs
+++ structures_vr_code/Assets/scripts/frame_TubeController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class frame_TubeController : MonoBehaviour
 {   
+    public GameObject frame;
+
     public void SetDimensions(float outsideDepth, float outsideWidth, float flangeThickness, float webThickness)
     {
-        Transform trans = gameObject.transform;
+        Transform trans = frame.transform;
         trans.localScale = new Vector3(outsideWidth, trans.localScale.y, outsideDepth);
     }
 }
