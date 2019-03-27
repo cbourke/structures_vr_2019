63305b097cfb75e61a7e75e13a8d5884ee92f879
James Griess <bobo12377@gmail.com>
2019-03-27 10:42:39 -0500

    Everything appears to be working with joint restraints

--- structures_vr_code/Assets/jointRestraintUI.cs
+++ structures_vr_code/Assets/jointRestraintUI.cs
@@ -8,12 +8,18 @@ public class jointRestraintUI : MonoBehaviour
     public TMP_Dropdown typeDropdown;
     public selectionController mySelectionController;
     public constructorController myConstructorController;
+    public TMP_Text deleteButtonText;
 
     private char type;
+    private bool isWait = false;
+    private string deleteText;
+    private string confirmText;
 
     // Start is called before the first frame update
     void Start()
     {
+        deleteText = deleteButtonText.text;
+        confirmText = "Sure?";
     }
 
     
@@ -30,10 +36,25 @@ public class jointRestraintUI : MonoBehaviour
     
     public void deleteJoints()
     {
-        // get selection
-        // create list of points
-        // call constructorcontroller
-        
+        if(isWait) {
+            // user has confirmed they want to delete
+            List<GridNode> nodes = mySelectionController.GetSelectedNodes();
+            foreach (GridNode n in nodes)
+            {
+                myConstructorController.deleteJointRestraint(n.getLocation());
+            }
+            StopCoroutine(deleteCheck());
+            deleteButtonText.text = deleteText;
+            isWait = false;
+        } else {
+            // delete button has not been pressed yet
+            deleteButtonText.text = confirmText;
+            StartCoroutine(deleteCheck());
+        }
+    }
+
+    public void deselectAll() {
+        mySelectionController.clearNodeSelection();
     }
 
     void getRestraintType() {
@@ -49,4 +70,15 @@ public class jointRestraintUI : MonoBehaviour
             Debug.LogError("Invalid restraint type");
         }
     }
+
+
+    IEnumerator deleteCheck()
+    {
+        isWait = true;
+        yield return new WaitForSeconds(5);
+        isWait = false;
+        deleteButtonText.text = deleteText;
+        
+    }
+
 }
