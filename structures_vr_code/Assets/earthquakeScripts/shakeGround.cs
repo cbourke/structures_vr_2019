d9c204856f94bc97b8390177f763d1bd43962519
Kameron Heyen <kameronh7@gmail.com>
2019-04-03 11:59:23 -0500

    Added button to toggle shaking behavior
    Added button to toggle shaking behavior

--- structures_vr_code/Assets/earthquakeScripts/shakeGround.cs
+++ structures_vr_code/Assets/earthquakeScripts/shakeGround.cs
@@ -1,36 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class shakeGround : MonoBehaviour
 {
     public float minThrust;
     public float maxThrust;
+    public float duration;
     public Rigidbody rb;
-    
+    public Button eqButton;
+    private bool eq = false;
 
     // Use this for initialization
     void Start () {
         rb = GetComponent<Rigidbody>();
-        Vector3 velocity = rb.velocity;
-        float forceX = Random.Range(minThrust, maxThrust);
-        Vector3 forceNew = new Vector3(forceX, 0, 0);
-
-        rb.AddForce(forceNew, ForceMode.Impulse);
+        eqButton.onClick.AddListener(() => shakeOnClick(minThrust, maxThrust));
     }
 
     // Update is called once per frame
     void Update () {
         //Debug.Log("update");
+        if (eq)
+        {
+            float forceZ = Random.Range(minThrust, maxThrust);
+            float forceX = Random.Range(minThrust, maxThrust);
+
+            Vector3 forceNew = new Vector3(forceX, 0, forceZ);
+           
+            rb.AddForce(forceNew, ForceMode.Impulse);
+        }
         
-        //float forceZ = Random.Range(minThrust, maxThrust);
-       
 
-        //forceX = -(forceX);
-        //forceZ = -Random.Range(minThrust, maxThrust);
+    }
 
-        //forceNew = new Vector3(forceX, 0, 0);
-        //rb.AddForce(forceNew, ForceMode.Impulse);
-
+    void shakeOnClick(float minT, float maxT)
+    {
+        float endTime = Time.time + duration;
+        Debug.Log("EQ Begin");
+        eq = !eq;
+        /*
+        while (endTime > Time.time)
+        {
+            float forceX = Random.Range(minT, maxT);
+            Vector3 forceNew = new Vector3(forceX, 0, 0);
+            rb.AddForce(forceNew, ForceMode.Impulse);
+        }
+        */
     }
 }
